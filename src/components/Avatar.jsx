19c9e8feedb688c432c310e4b67e1070ccefd3dc
priyamm1007-jpg <priyamm1007@gmail.com>
2026-08-26 13:15:58 +0530

    Fix avatar image cropping and sizing

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -10,12 +10,34 @@ export default function Avatar({
     <span
       className={`avatar avatar-${size}${src ? " avatar-image" : ""}`}
       aria-label={name}
+      style={{
+        position: "relative",
+        display: "inline-flex",
+        flexShrink: 0,
+        overflow: "hidden",
+        aspectRatio: "1 / 1",
+        borderRadius: "50%",
+      }}
     >
       {src ? (
         <img
           src={src}
           alt={name}
           loading="lazy"
+          draggable="false"
+          style={{
+            position: "absolute",
+            inset: 0,
+            display: "block",
+            width: "100%",
+            height: "100%",
+            minWidth: "100%",
+            minHeight: "100%",
+            maxWidth: "none",
+            maxHeight: "none",
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
         />
       ) : (
         initial
